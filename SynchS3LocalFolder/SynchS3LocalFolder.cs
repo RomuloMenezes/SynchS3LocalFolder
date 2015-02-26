7dfaba15a7148894b0830e309b55f98fc212f417
Romulo Menezes <romulo.menezes@gmail.com>
2015-02-26 21:07:39 +0000

    Project creation. Collecting contents from local folder and S3

---
+++ SynchS3LocalFolder/SynchS3LocalFolder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Amazon;
+using Amazon.S3;
+using Amazon.S3.IO;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+
+namespace SynchS3LocalFolder
+{
+    class SynchS3LocalFolder
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> LocalFiles = new Dictionary<string, string>();
+            Dictionary<string, string> S3Files = new Dictionary<string, string>();
+            AmazonS3Client client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
+
+            // ---------------------------------------------- Local Files ----------------------------------------------
+            string dirName = "D:\\openPDC\\Archive\\";
+
+            string[] fileEntries = Directory.GetFiles(dirName);
+            foreach (string sCurrFileName in fileEntries)
+            {
+                LocalFiles.Add(sCurrFileName, sCurrFileName);
+            }
+            // ---------------------------------------------------------------------------------------------------------
+
+            // ---------------------------------------------- S3 Files ----------------------------------------------
+            ListObjectsRequest request = new ListObjectsRequest();
+            request.BucketName = "pmu-data";
+            request.Prefix = "Archive";
+
+            ListObjectsResponse response = client.ListObjects(request);
+
+            foreach (S3Object entry in response.S3Objects)
+            {
+                S3Files.Add(entry.Key, entry.Key);
+            }
+            // ------------------------------------------------------------------------------------------------------
+        }
+    }
+}
