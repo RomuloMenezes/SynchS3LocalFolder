58928b807e45609360d49ed87055a23903e2ee2d
Romulo Menezes <romulo.menezes@gmail.com>
2015-02-27 20:08:10 +0000

    Adding the treatment of parameters. First version treatin source and target parameters

--- SynchS3LocalFolder/SynchS3LocalFolder.cs
+++ SynchS3LocalFolder/SynchS3LocalFolder.cs
@@ -19,30 +19,79 @@ namespace SynchS3LocalFolder
         {
             Dictionary<string, string> LocalFiles = new Dictionary<string, string>();
             Dictionary<string, string> S3Files = new Dictionary<string, string>();
+            Dictionary<string, string> FilesToCopy = new Dictionary<string, string>();
             AmazonS3Client client = new AmazonS3Client(Amazon.RegionEndpoint.USEast1);
+            int iBackSlashIndex;
+            string sLocalFilenameNoPath;
+            string sS3FilenameNoPath;
+            string sBucketName;
+            string sBucketPrefix;
+            string dirName = "";
 
-            // ---------------------------------------------- Local Files ----------------------------------------------
-            string dirName = "D:\\openPDC\\Archive\\";
-
-            string[] fileEntries = Directory.GetFiles(dirName);
-            foreach (string sCurrFileName in fileEntries)
+            if (args.Length == 0)
             {
-                LocalFiles.Add(sCurrFileName, sCurrFileName);
+                Console.WriteLine("Please select source and target folders");
+                return;
             }
-            // ---------------------------------------------------------------------------------------------------------
 
-            // ---------------------------------------------- S3 Files ----------------------------------------------
-            ListObjectsRequest request = new ListObjectsRequest();
-            request.BucketName = "pmu-data";
-            request.Prefix = "Archive";
-
-            ListObjectsResponse response = client.ListObjects(request);
-
-            foreach (S3Object entry in response.S3Objects)
+            if (args.Length == 1)
             {
-                S3Files.Add(entry.Key, entry.Key);
+                Console.WriteLine("Please select target folders");
+                return;
             }
-            // ------------------------------------------------------------------------------------------------------
+
+            if (args.Length == 2)
+            {
+                try
+                {
+                    if(args[0].Substring(0,5)=="s3://"||args[0].Substring(0,5)=="S3://")
+                    {
+                        iBackSlashIndex = args[0].IndexOf('/',5);
+                        sBucketName = args[0].Substring(5, iBackSlashIndex - 5);
+                        sBucketPrefix = args[0].Substring(iBackSlashIndex + 1, args[0].Length - iBackSlashIndex - 1);
+
+                        dirName = args[1];
+                    }
+                    else
+                    {
+                        dirName = args[0];
+
+                        iBackSlashIndex = args[1].IndexOf('/', 5);
+                        sBucketName = args[1].Substring(5, iBackSlashIndex - 5);
+                        sBucketPrefix = args[1].Substring(iBackSlashIndex + 1, args[1].Length - iBackSlashIndex - 1);
+                    }
+
+                    // ---------------------------------------------- Local Files ----------------------------------------------
+                    string[] fileEntries = Directory.GetFiles(dirName);
+                    foreach (string sCurrFileName in fileEntries)
+                    {
+                        iBackSlashIndex = sCurrFileName.LastIndexOf('\\') + 1;
+                        sLocalFilenameNoPath = sCurrFileName.Substring(iBackSlashIndex, sCurrFileName.Length - iBackSlashIndex);
+                        LocalFiles.Add(sLocalFilenameNoPath, sLocalFilenameNoPath);
+                    }
+                    // ---------------------------------------------------------------------------------------------------------
+
+                    // ---------------------------------------------- S3 Files ----------------------------------------------
+                    ListObjectsRequest request = new ListObjectsRequest();
+                    request.BucketName = sBucketName;
+                    request.Prefix = sBucketPrefix;
+
+                    ListObjectsResponse response = client.ListObjects(request);
+
+                    foreach (S3Object entry in response.S3Objects)
+                    {
+                        sS3FilenameNoPath = entry.Key.Substring(request.Prefix.Length + 1, entry.Key.Length - request.Prefix.Length - 1);
+                        if(sS3FilenameNoPath.Length > 0)
+                            S3Files.Add(sS3FilenameNoPath, sS3FilenameNoPath);
+                    }
+                    // ------------------------------------------------------------------------------------------------------
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Erro! " + e.Message);
+                    return;
+                }
+            }
         }
     }
 }
